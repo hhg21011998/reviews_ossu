2a597486bf49610e272bf68931ed68d75a4e009f
hhg21011998 <hhg21011998@gmail.com>
2022-09-15 16:18:18 +0700

    Update 13_web.py

--- 1.Intro_CS/PY4E_ossu-master/12_network.py
+++ 1.Intro_CS/PY4E_ossu-master/12_network.py
@@ -43,8 +43,8 @@ import re
 # mysock.close()
 
 
-# Exercise 3: Use urllib to replicate the previous exercise of (1) retrieving the document from a URL,
-# (2) displaying up to 3000 characters, and (3) counting the overall number of characters in the document.
+# Exerci, and (3) counting the ose 3: Use urllib to replicate the previous exercise of (1) retrieving the document from a URL,
+# (2) displaying up to 3000 charactersverall number of characters in the document.
 # Don’t worry about the headers for this exercise, simply show the first 3000 characters of the document contents.
 
 # url = input("Enter - ")
--- 1.Intro_CS/PY4E_ossu-master/13_web.py
+++ 1.Intro_CS/PY4E_ossu-master/13_web.py
@@ -60,20 +60,17 @@ import ssl
 
 # Autograder: Extracting Data from XML
 
-# url = input("Enter url: ")
-# if url != "http://py4e-data.dr-chuck.net/comments_1456361.xml":
-#     url = "http://py4e-data.dr-chuck.net/comments_1456361.xml"
-#     print("Url:", url)
+# url = "http://py4e-data.dr-chuck.net/comments_1636603.xml"
 # fhand = urllib.request.urlopen(url)
 # fxml = ""
 # for line in fhand:
 #     fxml += line.decode()
-#
+
 # tree = ET.fromstring(fxml)
 # lst = tree.findall("comments/comment")
 # print("Count:", len(lst))
 # sum = 0
-#
+
 # for item in lst:
 #     count = int(item.find("count").text)
 #     sum += count
@@ -83,48 +80,41 @@ import ssl
 
 # Autograder: Extract Data from JSON
 
-# url = input("Enter url: ")
-#
-# if url != "http://py4e-data.dr-chuck.net/comments_1456362.json":
-#     url = "http://py4e-data.dr-chuck.net/comments_1456362.json"
-#     print("Url:", url)
-#
-# fhand = urllib.request.urlopen(url) #return a <class 'http.client.HTTPResponse'>
-# # print(type(fhand))
-# data = fhand.read().decode() #return a <class 'str'>
-# # print(type(data))
-# js = json.loads(data) #return a <class 'dict'>
-# print(type(js))
-# sum = 0
-#
-# for item in js["comments"]:
-#     sum += int(item["count"])
-# print(sum)
+url = "http://py4e-data.dr-chuck.net/comments_1636604.json"
 
+fhand = urllib.request.urlopen(url) #return a <class 'http.client.HTTPResponse'>
+# print(type(fhand))
+data = fhand.read().decode() #return a <class 'str'>
+# print(type(data))
+js = json.loads(data) #return a <class 'dict'>
+sum = 0
+for item in js["comments"]:
+    sum += int(item["count"])
+print(sum)
 
 
 # Autograder: Using the GeoJSON API
 
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
+# # Ignore SSL certificate errors
+# ctx = ssl.create_default_context()
+# ctx.check_hostname = False
+# ctx.verify_mode = ssl.CERT_NONE
 
-key = 42
+# key = 42
 
-url = "http://py4e-data.dr-chuck.net/json?"
+# url = "http://py4e-data.dr-chuck.net/json?"
 
-address = input("Enter a address: ")
+# address = input("Enter a address: ")
 
-param = dict()
+# param = dict()
 
-param["address"] = address
-param["key"] = key
+# param["address"] = address
+# param["key"] = key
 
-jsonURL = url + urllib.parse.urlencode(param)
+# jsonURL = url + urllib.parse.urlencode(param)
 
-data = urllib.request.urlopen(jsonURL, context=ctx)
-dataString = data.read().decode()
+# data = urllib.request.urlopen(jsonURL, context=ctx)
+# dataString = data.read().decode()
 
-js = json.loads(dataString)
-print(js["results"][0]["place_id"])
+# js = json.loads(dataString)
+# print(js["results"][0]["place_id"])
